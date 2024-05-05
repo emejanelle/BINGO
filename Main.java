30c85633d08174a6425d7afac8a754562c3d0085
Micerey <104989213+Micerey@users.noreply.github.com>
2024-05-05 18:12:52 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,25 +1,9 @@
 import Controller.Logic;
-import Model.BingoModel;
 import View.UI;
 
 public class Main {
     public static void main(String[] args) {
         UI ui = new UI();
-        BingoModel[] bingoModels = new BingoModel[4];
-
-        // Create four Bingo cards
-        for (int i = 0; i < 4; i++) {
-            bingoModels[i] = new BingoModel();
-        }
-
-        // Extract cards from models
-        String[][][] cards = new String[4][][];
-        for (int i = 0; i < 4; i++) {
-            cards[i] = bingoModels[i].getCard();
-        }
-
-        // Print the four Bingo cards side by side
-        ui.printBingoCards(cards);
 
         Logic logic = new Logic(ui);
 
@@ -28,4 +12,3 @@ public class Main {
         } while (ui.promptToContinue()); // Check for continuation after each game round
     }
 }
-
